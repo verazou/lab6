9eba04cbbddfeb307e7ff070c5dbcbc50a8b16ac
Vera Zou <zouvra@gmail.com>
2017-02-16 09:47:17 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,32 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	$.get("/project/" + idNumber, addProject);
+
+	console.log("/project/" + idNumber);
+
 	console.log("User clicked on project " + idNumber);
+}
+
+function addProject(result){
+	console.log(result);
+
+	var detailHTML = '<img class="detailsImage" src=' + result["image"] + '></img>'
+	 + '<h4>' + result["title"] + '</h4>' + '<h5>' + result["date"] + '</h5>'
+	 + result["summary"];
+
+
+	$("#project" + result["id"] + " .details").html(detailHTML);
+
+}
+
+function randomizeColorsCSS(result){
+	var colors = result["colors"]["hex"];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
 
 /*
@@ -35,4 +60,5 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", randomizeColorsCSS);
 }
